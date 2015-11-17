c3f232aa4c97025cedba9e30d6325006b7ca6da3
Eric Pascual <eric@pobot.org>
2015-11-17 23:05:06 +0100

    first version

---
+++ setup-template.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='python-ev3dev-contrib',
+    version='%(version)s',
+    description='Python ev3dev contribution packages',
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    license='MIT',
+    include_package_data=True,
+    packages=['ev3dev', 'ev3dev.contrib'],
+    install_requires=['python-ev3dev-ep'],
+    package_dir={'': 'src'},
+    )
+
